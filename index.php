812f67565aabd6a4f9d10dcd4ada2f6cfced1d2d
Qais Patankar <qaisjp@gmail.com>
2019-05-31 02:11:24 +0100

    Minor whitespace tweaks

--- index.php
+++ index.php
@@ -70,7 +70,7 @@ function OpenURL(e, url){
    }
    return false;
 };
-</script>	
+</script>
 </head>
 <body>
 <form action="/">
@@ -80,29 +80,29 @@ function OpenURL(e, url){
   padding: 0 0 0 14px;
   height: 33px;">
   <span style='line-height: 33px;vertical-align: middle;'>
-  <a href="?">All</a>  
-       
+  <a href="?">All</a>
+
 	   &nbsp;
-  <a href="?Branch=master">Master</a>  
-       
+  <a href="?Branch=master">Master</a>
+
 	   &nbsp;
-  <a href="?Branch=1.5">1.5</a>  
-      
+  <a href="?Branch=1.5">1.5</a>
+
 	  &nbsp;
-  <a href="?Branch=1.4">1.4</a>  
-      
+  <a href="?Branch=1.4">1.4</a>
+
 	  &nbsp;
-	  
+
   </span>
   <div style="float:right;line-height: 33px;vertical-align: middle;padding-right: 20px;">
      <input name="SHA" placeholder="SHA filter" style="width:21em" value="<? echo $_GET['SHA']; ?>">
-	 
+
      <input name="Author" placeholder="Author filter" value="<? echo $_GET['Author']; ?>">
-       
+
      <input name="Branch" placeholder="Branch filter" value="<? echo $_GET['Branch']; ?>">
-	   
+
      <input name="Revision" placeholder="Revision filter" value="<? echo $_GET['Revision']; ?>">
-	   
+
      <input type="button" onclick="document.location='index.php';" value="Reset" />
      <input type="submit" value="Submit">
   </div>
@@ -112,9 +112,9 @@ function OpenURL(e, url){
  <div id="colcontrol">
 <div class="list">
 <div class="googlecodelink">
- 
+
  <?php
- 
+
 // Get our parameters
 $version = CheckParam('Branch');
 $revision = CheckParam('Revision');
@@ -182,7 +182,7 @@ else if ( $version == null )
 			$subquery = $subquery . $word . " Revision=(SELECT max(Revision) from mta_gitstuff.github)";
 			$word = " AND";
 		}
-		else 
+		else
 		{
 			$subquery = $subquery . $word . " Revision='" . mysqli_real_escape_string ( $conn, $revision )  . "'";
 			$word = " AND";
@@ -239,7 +239,7 @@ $result = $conn->query("SELECT COUNT(*) as Count FROM (SELECT Revision from mta_
 $count = 0;
 
 // make sure we have some rows
-if ($result->num_rows > 0) 
+if ($result->num_rows > 0)
 {
 	// fetch our row
     $row = $result->fetch_assoc();
@@ -258,8 +258,8 @@ if ($page != null && $page > $count && $count != 0)
 // calculate next and previous page
  $nextpage = $page + 1;
  $previouspage = $page - 1;
- 
- 
+
+
  // case 1: empty GET so no parameters
  if (empty($_GET))
  {
@@ -270,7 +270,7 @@ if ($page != null && $page > $count && $count != 0)
  else if ( !isset($_GET['Page']) )
  {
 	$NewerLink = $_SERVER['QUERY_STRING'] . "&Page=$previouspage";
-	
+
 	$OlderLink = $_SERVER['QUERY_STRING'] . "&Page=$nextpage";
  }
  // case 3: get query includes a page #
@@ -279,23 +279,23 @@ if ($page != null && $page > $count && $count != 0)
 	$NewerLink = str_replace("Page=$page", "Page=$previouspage", $_SERVER['QUERY_STRING']);
 	$OlderLink = str_replace("Page=$page", "Page=$nextpage", $_SERVER['QUERY_STRING']);
  }
- 
+
  // Create a variable to store our next / previous page tag string and build it up so we can print it at the top and bottom
  $nextPreviousPage = "";
- 
+
  // if we need to show the newer link
  if ($page > 1 ) {
-	$nextPreviousPage = $nextPreviousPage . "<a href=\"index.php?"; 
+	$nextPreviousPage = $nextPreviousPage . "<a href=\"index.php?";
 	$nextPreviousPage = $nextPreviousPage . $NewerLink;
 	$nextPreviousPage = $nextPreviousPage . "\"><b>&lsaquo;</b> Newer</a> ";
  }
  // show page #
  $nextPreviousPage = $nextPreviousPage . "Page " . $page . " of " . $count;
- 
+
  // show older link
  $nextPreviousPage = $nextPreviousPage . " <a href=\"index.php?";
  $nextPreviousPage = $nextPreviousPage . $OlderLink;
- 
+
  // show link to google code page
  if ($nextpage > $count && $count != 0)
  {
@@ -307,24 +307,24 @@ if ($page != null && $page > $count && $count != 0)
  }
  // print next/previous page at the top
  echo $nextPreviousPage;
- 
+
  ?>
- 
+
  </div>
 <b>Committed Changes</b>
 </div>
 <table class="results" id="resultstable">
   <tbody>
   <tr style="text-align:center">
-    <th style="width:7ex;text-align:center"><b>Rev</b></th>		
-    <th style="width:3.5em;text-align:center"><b>Avatar</b></th>		
-    <th style="text-align:center;padding-right:10px;padding-left:10px;"><b>Author</b></th>		
+    <th style="width:7ex;text-align:center"><b>Rev</b></th>
+    <th style="width:3.5em;text-align:center"><b>Avatar</b></th>
+    <th style="text-align:center;padding-right:10px;padding-left:10px;"><b>Author</b></th>
     <th style="text-align:center;padding-right:10px;padding-left:10px;"><b>Branch</b></th>
     <th style="width:80em;text-align:center"><b>Log Message</b></th>
     <th style="width:22em;text-align:center"><b>Date</b></th>
-    <th style="width:54ex;text-align:center"><b>SHA</b></th>	
+    <th style="width:54ex;text-align:center"><b>SHA</b></th>
   </tr>
-  
+
 <?php
 
 
@@ -357,7 +357,7 @@ if ($result->num_rows > 0) {
 	$bBorderChange = false;
 	// while we have a result
     while($row = $result->fetch_assoc()) {
-	
+
 		// odd  (impacts highlighting)
 		if ( $bTest == true )
 		{
@@ -370,13 +370,13 @@ if ($result->num_rows > 0) {
 		}
 		// invert bTest
 		$bTest = !$bTest;
-		
+
 		// if this is a new revision
 		if ($rev != $row["Revision"])
 		{
 			echo $border . "border-right: 1px solid #ccc;text-align:center;'>";
 			// output the revision cell
-			echo "<a href='?Revision=" . $row["Revision"] . "&amp;Branch=" . $version . "'>r" . $row["Revision"] . "</a>" . "</td>\n"; 
+			echo "<a href='?Revision=" . $row["Revision"] . "&amp;Branch=" . $version . "'>r" . $row["Revision"] . "</a>" . "</td>\n";
 			$rev = $row["Revision"];
 		}
 		else
@@ -384,29 +384,29 @@ if ($result->num_rows > 0) {
 			// blank revision cell
 			echo "<td style='border-top: 0px solid #ccc; border-bottom: 0px solid #ccc; border-right: 1px solid #ccc;'>" . "     </td>\n";
 		}
-		
+
 		if ( $bBorderChange == true )
 		{
 			$border = "<td style='border-top: 1px solid #ccc; border-bottom: 0px solid #ccc;";
 		}
 		$bBorderChange = true;
-		
+
 		// output our Author and his avatar and set the max column width
 		echo $border . "border-right: 0px solid #ccc;text-align:center;'><img style='vertical-align:middle;' src='" . $row["AuthorAvatarURL"] . "' height='25' alt='Avatar' /> </td>\n";
 		$splitAuthor = explode ( '@', $row["Author"] );
-		echo $border . "border-right: 0px solid #ccc;'>" . $splitAuthor[0] . "</td>\n"; 
-		
+		echo $border . "border-right: 0px solid #ccc;'>" . $splitAuthor[0] . "</td>\n";
+
 		// master branch is just null in the database
 		$modifiedVersion = $row["Version"] ? $row["Version"] : "master";
 		// output our Branch and set the max column width
-		echo $border . "border-right: 0px solid #ccc;text-align:center;'><a href='?Branch=" . $modifiedVersion . "&amp;Revision=" . $revision . "'>" . $modifiedVersion . "</a></td>\n"; 
-		
+		echo $border . "border-right: 0px solid #ccc;text-align:center;'><a href='?Branch=" . $modifiedVersion . "&amp;Revision=" . $revision . "'>" . $modifiedVersion . "</a></td>\n";
+
 		// output our Log Message, set the max column width and replace any new lines with br tags
-//		echo $border . "border-right: 0px solid #ccc;'>" . str_replace ( "\n", "<br /><br />", str_replace ("\n\n", "\n", str_replace ( '>', "&gt;", str_replace ( '<', "&lt;", $row["LogMessage"]) ) ) ) . "</td>\n"; 	
+//		echo $border . "border-right: 0px solid #ccc;'>" . str_replace ( "\n", "<br /><br />", str_replace ("\n\n", "\n", str_replace ( '>', "&gt;", str_replace ( '<', "&lt;", $row["LogMessage"]) ) ) ) . "</td>\n";
         // OMG - Fiddled with by *someone*
-        echo $border . "border-right: 0px solid #ccc;padding-left:2em;'>"; 
+        echo $border . "border-right: 0px solid #ccc;padding-left:2em;'>";
         $lineList = array_filter( explode( "\n", $row["LogMessage"] ) );
-		
+
 		$i = 0;
 		// Indent wrapped lines, 5px between lines
         foreach( $lineList as $line )
@@ -417,18 +417,18 @@ if ($result->num_rows > 0) {
 				echo "...";
 				break;
 			}
-			echo "<span style='display: block; padding-left: 0.80em; text-indent:-0.80em; margin: 5px 0;'>"; 
-            echo "• " . $line . "<br />"; 
+			echo "<span style='display: block; padding-left: 0.80em; text-indent:-0.80em; margin: 5px 0;'>";
+            echo "• " . $line . "<br />";
 			echo "</span>";
         }
-        echo "</td>\n"; 
+        echo "</td>\n";
 
 		// output our Date and set the max column width
-		echo $border . "border-right: 0px solid #ccc;'>" . $row["Date"] . "</td>"; 
-		
+		echo $border . "border-right: 0px solid #ccc;'>" . $row["Date"] . "</td>";
+
 		// output our SHA and set the max column width
-		echo $border . "border-right: 0px solid #ccc;'>" . $row["SHA"] . "</td>\n"; 
-		
+		echo $border . "border-right: 0px solid #ccc;'>" . $row["SHA"] . "</td>\n";
+
 		// end of row
 		echo "</tr>\n";
 	}
@@ -458,11 +458,11 @@ $conn->close();
  </table>
  </div>
  </div>
- 
+
  <div class="bottomdiv">
 		<div class="listbottom">
 			<div class="googlecodelink">
-				<?php 
+				<?php
 					// print our next / previous page tag at the bottom
 					echo $nextPreviousPage;
 				?>
@@ -470,6 +470,6 @@ $conn->close();
 			<b>End of Committed Changes</b>
 		</div>
  </div>
- 
+
  </body>
  </html>
